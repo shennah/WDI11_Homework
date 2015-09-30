fd7d224656601fbe105f8ff66ec8caa3132b5319
Yilan Zhou <yilan.zhou@users.noreply.github.com>
2015-10-01 09:51:45 +1000

    with reward

--- yilan_zhou/week_01/w1-3_word_guesser.js
+++ yilan_zhou/week_01/w1-3_word_guesser.js
@@ -1,8 +1,9 @@
 var s = "fantastic"; //word to be guessed
 var w=s.split('');
 var guessedW=[];
-var found;
 var userGuess = "abcdefghijklmnopqrstuvwxyz".split('');
+var reward = 0; //reward balance
+var NumRepeat = 0; //char repeat in the word
 
 //populate guessed array
 for (var i = 0; i < w.length; i++) {
@@ -10,20 +11,27 @@ for (var i = 0; i < w.length; i++) {
 }
 
 function guessLetter(l) {
+    var rand = parseFloat(Math.random()*100).toFixed(0);
     if (w.indexOf(l)===-1) {
+        reward = reward - rand;
         return l + " is not found, guess again.";
+        
     } else {
         for (var x=0;x<w.length;x++) {
             if(w[x]===l) {
                 guessedW[x]=l;
+                NumRepeat = NumRepeat +1;
             }
         }
+        reward = reward + rand*NumRepeat;
+        NumRepeat = 0;
         return l + " is found";
     }
 }
 
 for (var i =0; i<userGuess.length; i++) {
    console.log(guessLetter(userGuess[i]));
+   console.log("Reward Balance: $" + reward);
     if (w.join() === guessedW.join()) {
         console.log("congrats");
         console.log(guessedW.join());
