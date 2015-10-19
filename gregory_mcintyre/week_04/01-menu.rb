76c562cfd311b1b75af38bbb1fc53090587000a1
Gregory McIntyre <greg@gregorymcintyre.com>
2015-10-19 15:27:34 +1100

    Updated menu example.

--- gregory_mcintyre/week_04/01-menu.rb
+++ gregory_mcintyre/week_04/01-menu.rb
@@ -4,10 +4,13 @@ loop do
   puts "Choose an option"
   puts " 1) +"
   puts " 2) -"
+  puts " 3) Quit"
 
   # ask which option the user wants
   choice = gets.to_i
   if choice == 1
     # they want to add numbers
+  elsif choice == 3
+    break # quit
   end
 end
