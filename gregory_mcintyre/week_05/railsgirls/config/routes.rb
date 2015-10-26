d9fb03a5d267a09d7250464a8daeb8325bcf1ace
Gregory McIntyre <greg@gregorymcintyre.com>
2015-10-26 17:05:53 +1100

    Homework hints.

--- gregory_mcintyre/week_05/railsgirls/config/routes.rb
+++ gregory_mcintyre/week_05/railsgirls/config/routes.rb
@@ -12,4 +12,14 @@ Rails.application.routes.draw do
   #           PUT    /ideas/:id(.:format)      ideas#update
   #           DELETE /ideas/:id(.:format)      ideas#destroy
 
+
+  # HOMEWORK
+
+  # /games/magic8?q=am+i+awesome
+
+  #get '/games/magic' => 'magic#question'
+
+  get '/games/secret' => 'secret_number#index'         # print a list of links
+  get '/games/secret/:number' => 'secret_number#guess' # validate the guess
+
 end
