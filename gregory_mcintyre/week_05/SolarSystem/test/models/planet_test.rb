2d0ec93a99bcc6ec688ba1a27df69b3f8ac969b6
Gregory McIntyre <greg@gregorymcintyre.com>
2015-10-29 17:11:51 +1100

    Extra sample code in Solar System app

--- gregory_mcintyre/week_05/SolarSystem/test/models/planet_test.rb
+++ gregory_mcintyre/week_05/SolarSystem/test/models/planet_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: planets
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  image       :string
+#  num_moons   :integer
+#  mean_radius :float
+#  mass        :float
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  orbit       :float
+#
+
 require 'test_helper'
 
 class PlanetTest < ActiveSupport::TestCase
