9d4631991c5ebfea63a8af4ecae319257f43b6c6
Gregory McIntyre <greg@gregorymcintyre.com>
2015-11-13 15:15:05 +1100

    Styling and minor improvements.

--- gregory_mcintyre/week_07/secrets/app/assets/javascripts/views/appview.js
+++ gregory_mcintyre/week_07/secrets/app/assets/javascripts/views/appview.js
@@ -3,6 +3,8 @@ var app = app || {};
 app.AppView = Backbone.View.extend({
 	el: '#app',
 
+	textFieldSelector: '.secret-content input',
+
 	events: {
 		'submit .secret-form': 'onSubmit',
 	},
@@ -12,15 +14,16 @@ app.AppView = Backbone.View.extend({
 			var view = new app.SecretView({model: secret});
 			view.render();
 		});
+		this.$(this.textFieldSelector).focus();
 	},
 
 	onSubmit: function(event) {
 		// don't do the normal form submit behaviour
-        event.preventDefault(); 
+		event.preventDefault();
 
-        var $textarea = $(event.target).find('.secret-content input');
-		var content = $textarea.val();
-		$textarea.val('').focus();
+		var $input = $(event.target).find(this.textFieldSelector);
+		var content = $input.val();
+		$input.val('').focus();
 		app.secrets.create({content: content});
 	}
 });
