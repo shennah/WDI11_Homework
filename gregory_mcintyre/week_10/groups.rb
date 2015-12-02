65f75a4aaa3c2a2a2fc787c31d8c80331805e1db
Gregory McIntyre <greg@gregorymcintyre.com>
2015-12-02 11:24:42 +1100

    Improve groups script.

--- gregory_mcintyre/week_10/groups.rb
+++ gregory_mcintyre/week_10/groups.rb
@@ -4,7 +4,7 @@ people = %w(Yilan Charley Andrew Daisy Donald Shennah Eric)
 people.shuffle!
 
 num_groups = 3
-groups = Array.new(3) { [] }
+groups = Array.new(num_groups) { [] }
 
 people.each_with_index do |person, index|
   groups[index % groups.size].push(person)
