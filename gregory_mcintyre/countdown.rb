206907c0d175143024f9ef68cb515aa3c1edc4b4
Gregory McIntyre <greg@gregorymcintyre.com>
2015-12-07 10:30:42 +1100

    Countdown timer for warm ups.

---
+++ gregory_mcintyre/countdown.rb
@@ -0,0 +1,12 @@
+require 'time'
+
+deadline = Time.parse(ARGV.first)
+
+while Time.now < deadline
+  s = deadline - Time.now
+  m, s = s.divmod(60)
+  print "\e[1J"
+  str = format("%02d:%02d", m, s)
+  system('figlet', '-f', 'larry3d', str) || puts(str)
+  sleep 1
+end
