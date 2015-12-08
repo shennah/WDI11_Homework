b305f3f76b228fcb949a22b1e42b8fefcbc25193
ButenkoT <tanya@butenko.me>
2015-12-08 19:28:35 +1100

    crypto

---
+++ warmup_exercises/week_11/crypto_square/crypto_square.rb
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Crypto
+
+#   attr_reader :phrase
+
+#   def initialize(phrase)
+#     @phrase = phrase
+#   end
+
+#   def normalize_plaintext
+#     phrase.gsub(/[^0-9a-zA-Z]/, "").downcase
+#   end
+
+#   def size
+#     Math.sqrt(normalize_plaintext.length).ceil
+#   end
+
+#   def plaintext_segments
+#     normalize_plaintext.scan(/.{1,#{size}}/)
+#   end
+
+#   def ciphertext
+#     cipher_text = []
+#     plaintext_segments.each do |segment|
+#       segment.chars.each_with_index do |letter, i|
+#         cipher_text[i] ||= []
+#         cipher_text[i] << letter
+#       end
+#     end
+#     cipher_text.join
+#   end
+
+#   def normalize_ciphertext
+#     ciphertext.scan(/.{1,5}/).join(" ")
+#   end
+# end
