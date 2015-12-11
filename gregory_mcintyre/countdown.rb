ae63d15f97c7a6ec780a5876a7c5e07db687b6d9
Gregory McIntyre <greg@gregorymcintyre.com>
2015-12-11 11:04:29 +1100

    Let countdown.rb font be overridden on command line.

--- gregory_mcintyre/countdown.rb
+++ gregory_mcintyre/countdown.rb
@@ -1,12 +1,14 @@
 require 'time'
 
 deadline = Time.parse(ARGV.first) rescue (Time.now + 60*60)
+font = ARGV[1] || 'fraktur'
+p deadline
 
 while Time.now < deadline
   s = deadline - Time.now
   m, s = s.divmod(60)
   print "\e[1J"
   str = format("%02d:%02d", m, s)
-  system('figlet', '-f', 'larry3d', str) || puts(str)
+  system('figlet', '-f', font, str) || puts(str)
   sleep 1
 end
